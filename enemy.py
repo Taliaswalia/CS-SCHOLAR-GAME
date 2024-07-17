91e051c915b807d2d08466405bed47d24984af22
Natalia <160363625+Taliaswalia@users.noreply.github.com>
2024-07-17 15:33:05 -0400

    better colison

--- enemy.py
+++ enemy.py
@@ -1,26 +1,31 @@
 import pygame
 import math
 import random
+from hitbox import Hitbox
 
 
-screenWidth = 400
-screenHeight = 300
-width = 40
+screenWidth = 700
+screenHeight = 600
+width = 60
 speed = 7
 
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, xpos, ypos, finalPosx, size):
         super(Enemy, self,).__init__()
         #random.choice
         self.size = size
-        self.enemyShape = [pygame.image.load('3.png'),pygame.image.load('4.png'),pygame.image.load('5.png')]
+        self.enemyShape = [pygame.image.load('triangleEn.png'),pygame.image.load('hexagonEn.png'),pygame.image.load('octogonEn.png'), pygame.image.load('squareEn.png'),pygame.image.load('pentagonEn.png')]
         self.x = random.choice(self.enemyShape)
-        self.surf = pygame.transform.scale(random.choice(self.enemyShape),(self.size, self.size))
-      # self.x = 7
+        self.surf = pygame.transform.scale(self.x,(self.size, self.size+10))
+        self.xpos = xpos
+        self.ypos = ypos
+
         self.finalPosx = finalPosx
-      # self.surf.fill((18, 107, 4))
-      # self.surf = pygame.Surface((self.size, self.size))
+        # self.surf = pygame.Surface((self.size, self.size))
+        # self.surf.fill((18, 107, 4))
+        
         
       #  self.surf.fill((95,87,120))
         self.rect = self.surf.get_rect (center=(xpos,ypos))
@@ -32,15 +37,14 @@ class Enemy(pygame.sprite.Sprite):
       if round(dist) > 1:
         dx, dy = dx / dist, dy / dist  # Normalize.
         # Move along this normalized vector towards the player at current speed.
-        #self.x = self.x *0.5self
-        self.rect.x += dx * 1.0002
-        self.rect.y += dy * 1.0002
 
-        self.size += 1.5
+        self.xpos += dx * 1.002
+        self.ypos += dy * 1.002
+
+        self.size *= 1.02
         self.surf = pygame.transform.scale(self.x, (self.size, self.size))
-        # size = round((width*(self.x))*2)
-        # self.surf = pygame.transform.scale(random.choice(self.enemyShape),(size, size))
-        #self.surf.fill((18, 107, 4))
+        self.rect = self.surf.get_rect (center=(self.xpos,self.ypos))
+        
 
       else:
         self.kill()
