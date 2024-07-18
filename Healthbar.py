c7c9ee2665643ffe462b612f2f520c89694fbb10
Natalia <160363625+Taliaswalia@users.noreply.github.com>
2024-07-18 10:52:38 -0400

    health smaller

--- Healthbar.py
+++ Healthbar.py
@@ -6,9 +6,9 @@ class Health_Bar(pygame.sprite.Sprite):
         super(Health_Bar, self).__init__()
         self.surf = pygame.Surface((100, 20))  # Placeholder for health bar
         self.rect = self.surf.get_rect(topleft=(x, y))
-        self.health = 100
+        self.health = 50
 
     def update(self, health):
         self.health = health
         self.surf.fill((191, 29, 29))  # Red color for health bar background
-        pygame.draw.rect(self.surf, (32, 128, 57), (0, 0, self.health, 20))  # Green part of the health bar# import pygame
+        pygame.draw.rect(self.surf, (32, 128, 57), (0, 0, self.health*2, 20))  # Green part of the health bar# import pygame
